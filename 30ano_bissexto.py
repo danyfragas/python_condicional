bbda412d7fcce055079f8ca23761dede7284079d
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-22 22:52:44 -0300

    Create 30ano_bissexto.py

---
+++ 30ano_bissexto.py
@@ -0,0 +1,8 @@
+# exercício 30: escreva um programa que leia um ano qualquer e mostre se ele é bissexto
+
+ano = int(input('Informe o ano qualquer: '))
+
+if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
+    print(f'O ano {ano} é bissexto.')
+else:
+    print(f'O ano {ano} não é bissexto.')
