d9d076b7e943118bcbee573797cdf9c90cce905a
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-22 22:53:24 -0300

    Create 31maior_menor.py

---
+++ 31maior_menor.py
@@ -0,0 +1,21 @@
+# exercício 31: escreva um programa que leia três números e mostre qual é o maior e menor.
+
+n1 = float(input('Número 1: '))
+n2 = float(input('Número 2: '))
+n3 = float(input('Número 3: '))
+
+if n1 >= n2 and n1 >= n3:
+    maior = n1
+elif n2 >= n1 and n2 >= n3:
+    maior = n2
+else:
+    maior = n3
+
+if n1 <= n2 and n1 <= n3:
+    menor = n1
+elif n2 <= n1 and n2 <= n3:
+    menor = n2
+else:
+    menor = n3
+
+print(f'O maior número é o {maior}, e o menor é {menor}.')
