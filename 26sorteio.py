09a704e809b9a54a5ff45241aab6556aca9475df
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-23 10:18:57 -0300

    Update 26sorteio.py

--- 26sorteio.py
+++ 26sorteio.py
@@ -1,6 +1,9 @@
 # exercício 26: escreva um programa que faça o computador "pensar" em um nº int entre 0 e 5 e peça para o usuário tentar descobrir qual foi o nº escolhido pelo computador. O programa deverá escrever na tela se ganhou ou perdeu.
 
 import random
+# from random improt randit
+
+# sorteio = randit [0, 5]
 num = [0, 1, 2, 3, 4, 5]
 sorteio = random.choice(num)
 
