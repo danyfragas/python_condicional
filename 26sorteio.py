38118976fdf02912f69eb14e51b95c3cc8932f7c
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-22 22:50:18 -0300

    Create 26sorteio.py

---
+++ 26sorteio.py
@@ -0,0 +1,13 @@
+# exercício 26: escreva um programa que faça o computador "pensar" em um nº int entre 0 e 5 e peça para o usuário tentar descobrir qual foi o nº escolhido pelo computador. O programa deverá escrever na tela se ganhou ou perdeu.
+
+import random
+num = [0, 1, 2, 3, 4, 5]
+sorteio = random.choice(num)
+
+n = int(input('Insira o número: '))
+print('O número sorteado foi: {}'. format(sorteio))
+
+if n == sorteio:
+    print('Você ganhou! C:')
+else:
+    print('Você perdeu! :C')
