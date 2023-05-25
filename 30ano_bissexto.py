0c6c0df01ab523437387d6c31b646ba162855ec6
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-25 13:51:16 -0300

    Update 30ano_bissexto.py

--- 30ano_bissexto.py
+++ 30ano_bissexto.py
@@ -1,8 +1,13 @@
 # exercício 30: escreva um programa que leia um ano qualquer e mostre se ele é bissexto
 
+from datetime import date
+
 ano = int(input('Informe o ano qualquer: '))
 
-if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
+if ano == 0:
+    ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'O ano {ano} é bissexto.')
 else:
     print(f'O ano {ano} não é bissexto.')
