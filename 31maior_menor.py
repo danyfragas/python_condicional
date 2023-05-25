18c98f38908bfa8c5d004bd7e22923c12349db16
Danielle Fragas <114834495+danyfragas@users.noreply.github.com>
2023-05-25 13:58:44 -0300

    Update 31maior_menor.py

--- 31maior_menor.py
+++ 31maior_menor.py
@@ -4,6 +4,19 @@ n1 = float(input('Número 1: '))
 n2 = float(input('Número 2: '))
 n3 = float(input('Número 3: '))
 
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+maior = n2
+if n1 > n2 and n1 > n2:
+    maior = n1
+if n3 > n2 and n3 > n1:
+    maior = n3
+
+'''
 if n1 >= n2 and n1 >= n3:
     maior = n1
 elif n2 >= n1 and n2 >= n3:
@@ -17,5 +30,7 @@ elif n2 <= n1 and n2 <= n3:
     menor = n2
 else:
     menor = n3
+'''
 
 print(f'O maior número é o {maior}, e o menor é {menor}.')
+
